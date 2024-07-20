73fab2fb4edcc0d47bd207928f3833db6dcbbe54
Mohamed Amged <mohamedamged55555@gmail.com>
2024-07-20 18:54:42 +0300

    add scrollToTop functions to onClick in categories

--- src/components/categoryOctagonShapeSec/CategoryOctagonShape.jsx
+++ src/components/categoryOctagonShapeSec/CategoryOctagonShape.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import './categoryOcatgonShape.css'
 import { NavLink, useNavigate } from 'react-router-dom'
+import { scrollToTop } from '../../functions/scrollToTop';
 export default function CategoryOctagonShape({ octagonIcon, iconName, iconLink }) {
     const navigate = useNavigate();
 
     return (
         <div className='CategoryOctagonShape__handler'>
             <div className="octagonItem__handler">
-                <div className="octagon__shape" onClick={()=>navigate(`${iconLink}`)}>
+                <div className="octagon__shape"
+                    onClick={() => {
+                        navigate(`${iconLink}`);
+                        scrollToTop();
+                    }}>
                     <img src={octagonIcon} alt="icon" />
                 </div>
-                <NavLink to={iconLink} className='nav-link'>
+                <NavLink
+                    onClick={() => {
+                        scrollToTop();
+                    }} to={iconLink} className='nav-link'>
                     <span>{iconName}</span>
                 </NavLink>
             </div>
