8e0ee2234745120286d58fce167a9507711d4724
Mohamed Amged <mohamedamged55555@gmail.com>
2024-08-27 02:55:58 +0300

    inialize add new role logic and add the preferd permission for the role

--- src/components/addRoleSec/AddRole.jsx
+++ src/components/addRoleSec/AddRole.jsx
@@ -1,9 +1,132 @@
-import React from 'react'
+import { zodResolver } from '@hookform/resolvers/zod';
+import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { AddRoleSchema } from '../../validation/AddRoleSchema';
+import { baseURL } from '../../functions/baseUrl';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { scrollToTop } from '../../functions/scrollToTop';
 
-export default function AddRole() {
+export default function AddRole({token}) {
+  const loginType = localStorage.getItem('loginType');
+  const [permissions ,setPermissions] = useState([]);
+  const [accepetedPermissions,setAcceptedPermissions] = useState([]);
+
+  const gettingAllPermissions = async () => {
+    await axios.get(`${baseURL}/${loginType}/permissions`,{
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        Authorization: `Bearer ${token}`
+      }
+    }).then((response) => {
+      setPermissions(response?.data?.data?.permissions);
+    }).catch(error=>{
+      toast.error(error?.response?.data?.message);
+    });
+  };
+
+  useEffect(()=>{
+    gettingAllPermissions();
+  },[]);
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors , isSubmitting}
+} = useForm({
+    defaultValues: {
+      name: '',
+    },
+    resolver: zodResolver(AddRoleSchema)
+  });
+
+  const onSubmit = async (data) => {
+    const toastId = toast.loading('Loading...');
+    if(accepetedPermissions.length > 0){
+      data.permission_id = accepetedPermissions;
+      axios.post(`${baseURL}/${loginType}/roles-create?t=${new Date().getTime()}`,data,{
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        }
+      })
+      .then(response => {
+        scrollToTop();
+        window.location.reload();
+        toast.success(response?.data?.message || 'Role Added Successfully!',{
+          id: toastId,
+          duration: 1000,
+        });
+      })
+      .catch(error => {
+        if(error?.response?.data?.errors){
+          Object.keys(error?.response?.data?.errors).forEach((key) => {
+            setError(key, {message: error?.response?.data?.errors[key][0]});
+          });
+        };
+        toast.error(error?.response?.data.message || 'Validation Error!',{
+          id: toastId,
+          duration: 1000,
+        });
+      })
+    }else {
+      toast.error('You Should Add at least One Permission!',{
+        id: toastId,
+        duration: 2000,
+      })
+    };
+  };
+
+  const handleChangeCheckBox = (e,id) => {
+    if(e.target.checked === true){
+      setAcceptedPermissions([...accepetedPermissions,id]);
+    }else if(e.target.checked === false){
+      setAcceptedPermissions(accepetedPermissions?.filter(el => +el !== +id));
+    };
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mt-2 ms-2 profileFormInputItem mb-3">
+        <label htmlFor="dashboardCompanyAddRoleName">Role Name</label>
+        <input
+          id='dashboardCompanyAddRoleName'
+          placeholder='New Role Name'
+          className={`form-control signUpInput mt-2 ${errors?.name ? 'inputError' : ''}`}
+          {...register('name')}
+          type="text"
+        />
+        {
+          errors?.name
+          &&
+          (<span className='errorMessage'>{errors?.name?.message}</span>)
+        }
+      </div>
+      <div className="allPermission__items mt-4">
+        {
+          permissions?.map((perm) =>{
+            return(
+              <div key={perm?.id}>
+                <label className="switch cursorPointer w-100 px-3 d-flex justify-content-between gap-3 align-items-center mb-3">
+                <span>
+                  {perm?.name}
+                </span>
+                  <input
+                    type="checkbox"
+                    onChange={(e)=>handleChangeCheckBox(e,perm?.id)}
+                  />
+                  <span className="slider"></span>
+                </label>
+              </div>
+          )})
+        }
+      </div>
+      <div className="submitAddRole text-center my-4">
+        <input type="submit" disabled={isSubmitting} value="Confirm Changes" className='updateBtn mt-0' />
+      </div>
+    </form>
+  );
+};
--- src/components/myDefaultRolesSec/MyDefaultRoles.jsx
+++ src/components/myDefaultRolesSec/MyDefaultRoles.jsx
@@ -3,131 +3,49 @@ import './myDefaultRoles.css'
 import axios from 'axios';
 import { baseURL } from '../../functions/baseUrl';
 import toast from 'react-hot-toast';
-import Cookies from 'js-cookie';
-
-// Cookies.remove('UpdatedRuleData');
 
 export default function MyDefaultRoles({token}) {
     const loginType = localStorage.getItem('loginType');
-    const [permissions, setPermissions] = useState(Cookies.get('Permissions') ? 
-    JSON.parse(Cookies.get('Permissions')) 
-    : 
-        []
-    );
-    const [allRoles,setAllRoles] = useState(Cookies.get('AllRoles') ?
-            JSON.parse(Cookies.get('AllRoles')) 
-        : 
-            []
-    );
+    const [permissions, setPermissions] = useState([]);
+    const [allRoles,setAllRoles] = useState([]);
     const [loadingPermissions,setLoadingPermissions] = useState(false);
-    // const [roles,setRoles] = useState(Cookies.get('Roles&Permissions') ?
-    //         JSON.parse(Cookies.get('Roles&Permissions')) 
-    //     : 
-    //         []
-    // );
-    const [changedPermsIds,setChangedPermsIds] = useState([]);
-let counter = 0;
+    let counter = 0;
 
-    const handleDeleteThisRule = async (id) => {
-        await axios.delete(`${baseURL}/${loginType}/roles-delete/${id}`,{
+    const gettingAllRoles = async ()=>{
+        axios.get(`${baseURL}/${loginType}/roles?t=${new Date().getTime()}`,{
             headers: {
-                'Content-Type': 'application/json',
+                'content-type': 'application/json',
                 'Accept': 'application/json',
                 Authorization: `Bearer ${token}`
             }
         }).then((response) => {
-            Cookies.set('AllRoles', JSON.stringify(allRoles?.filter(el => +el?.id !== +id)));
-            // Cookies.set('Roles&Permissions', JSON.stringify(roles?.filter(rule => +rule?.role?.id !== +id)));
-            Cookies.set('Permissions', JSON.stringify(permissions?.filter(perm => +perm?.permissionId !== +id)));
-            toast.success(response?.data?.message);
+            setAllRoles(prevRoles => {
+                const lastShape = [...prevRoles , ...response?.data?.data?.roles];
+                return lastShape;
+            });
         }).catch(error=>{
             toast.error(error?.response?.data?.message);
         });
     };
 
     useEffect(()=>{
-        if(!Cookies.get('AllRoles')){
-            (async ()=>{
-                axios.get(`${baseURL}/${loginType}/roles`,{
-                    headers: {
-                        'content-type': 'application/json',
-                        'Accept': 'application/json',
-                        Authorization: `Bearer ${token}`
-                    }
-                }).then((response) => {
-                    setAllRoles(prevRoles => {
-                        const lastShape = [...prevRoles , ...response?.data?.data?.roles];
-                        return lastShape;
-                    });
-                    Cookies.set('AllRoles', JSON.stringify(response?.data?.data?.roles));
-                }).catch(error=>{
-                    toast.error(error?.response?.data?.message);
-                });
-            })();
-        }
-    },[allRoles,Cookies.get('AllRoles')]);
-
-    const handleUpdateRule = async (id) => {
-        // const cookiesValue = Cookies.get('UpdatedRuleData');
-        // if(cookiesValue){
-        //     const parsedData = JSON.parse(cookiesValue);
-        //     const uniquePermissionIds = [...new Set(parsedData[0]?.permission_id)];
-        //     const requesData = {
-        //         name: parsedData[0]?.name,
-        //         permission_id: uniquePermissionIds
-        //     };
-        //     const formData = new FormData();
-        //     Object.keys(requesData).forEach((key) => {
-        //         if(key === 'permission_id'){
-        //             requesData[key].forEach((item, index) => {
-        //                 formData.append(`${key}[${index}]`, item);
-        //             });
-        //         }
-        //         formData.append(key, requesData[key]);
-        //     });
-        //     const toastId = toast.loading('Updating Role...')
-        //     await axios.post(`${baseURL}/${loginType}/roles-update/${id}`, formData , {
-        //         headers: {
-        //             'Accept': 'application/json',
-        //             'Content-Type': 'multipart/form-data',
-        //             'Authorization': `Bearer ${token}`,
-        //         }
-        //     })
-        //     .then((response)=>{
-        //         Cookies.set('Permissions',permissions);
-        //         console.log(response?.data?.data);
-        //         toast.success(response?.data?.message,{
-        //             id: toastId,
-        //             duration: 1000
-        //         });
-        //     })
-        //     .catch(error=>{
-        //         console.log(error?.response);
-        //         toast.error(error?.data?.data?.message || 'Error',{
-        //             id: toastId,
-        //             duration: 1000
-        //         })
-        //     });
-        // }
-    };
+        if(allRoles.length === 0){
+            gettingAllRoles();
+        };
+    },[allRoles]);
 
     const gettingSingleRulePermissions = async (id) => {
         if(id && !permissions?.find(el => +el?.permissionId === +id)){
             setPermissions([]);
             setLoadingPermissions(true);
-            await axios.get(`${baseURL}/${loginType}/roles-show/${id}`,{
+            await axios.get(`${baseURL}/${loginType}/roles-show/${id}?t=${new Date().getTime()}`,{
                 headers: {
                     'Content-Type': 'application/json',
                     'Accept': 'application/json',
                     Authorization: `Bearer ${token}`
                 }
             }).then((response) => {
-                // setRoles((prevRoles) => {
-                //     const updatedRoles = [...prevRoles, response?.data?.data];
-                //     // Cookies.set('Roles&Permissions', JSON.stringify(updatedRoles));
-                //     return updatedRoles;
-                // });
-                setPermissions([{[id]: response?.data?.data?.permissions, 'permissionId': id }]);
+                setPermissions(response?.data?.data?.permissions);
                 setLoadingPermissions(false);
             }).catch(error=>{
                 toast.error(error?.response?.data?.message);
@@ -135,60 +53,9 @@ let counter = 0;
         };
     };
 
-    const gettingDefaultPermissions = (roles) => {
-        roles?.map(rule => gettingSingleRulePermissions(rule?.id));
-    }
-
-    // useEffect(()=>{
-    //     if(allRoles.length > 0 && !Cookies.get('Roles&Permissions') && !Cookies.get('Permissions')){
-    //         gettingDefaultPermissions(allRoles);
-    //     };
-    // },[allRoles]);
-
     const [expandedRole, setExpandedRole] = useState(null);
 
-    // const togglePermission = (id , ruleId) => {
-    //     const currPerms = permissions?.map(perm =>
-    //         (+perm?.permissionId === +ruleId) ?
-    //         {[ruleId] : perm?.[ruleId]?.map(el =>
-    //             (+el?.id === +id) ?
-    //             {...el , enable: !el.enable}
-    //             :
-    //             el ),
-    //             permissionId: ruleId
-    //         }
-    //         :
-    //         perm
-    //     );
-
-    //     let ids = [];
-    //     const currentRuleUnderChanging = allRoles?.find(rule => +rule?.id === +ruleId).name;
-    //         permissions?.map(perm => 
-    //             (+perm?.permissionId === +ruleId) ? 
-    //                 (changedPermsIds?.find(el => +el === +id)) ? 
-    //                     <>
-    //                         {setChangedPermsIds(changedPermsIds?.filter(el => +el !== +id))}
-    //                         {ids.push(...changedPermsIds?.filter(el => +el !== +id))}
-    //                     </>
-    //                 :
-    //                     <>
-    //                         {setChangedPermsIds([...changedPermsIds , id])}
-    //                         {ids.push(...changedPermsIds , id)}
-    //                     </>
-    //             :
-    //                 perm
-    //         );
-    //         Cookies.set('UpdatedRuleData' , JSON.stringify([{name: currentRuleUnderChanging , permission_id: ids }]));
-    //     setPermissions(currPerms);
-    // };
-
     const toggleRole = (id) => {
-        // setPermissions([]);
-        // setChangedPermsIds([]);
-        // Cookies.remove('UpdatedRuleData');
-        // if(toggledOnce){
-            // gettingDefaultPermissions(allRoles);
-        // }
         counter = 0;
         gettingSingleRulePermissions(id);
         setExpandedRole(prevId => (prevId === id ? null : id));
@@ -197,6 +64,23 @@ let counter = 0;
     const getStyle = (enable) => ({
         color: enable ? '#000' : 'rgba(0, 0, 0, 0.3)'
     });
+
+    const handleDeleteThisRule = async (id) => {
+        await axios.delete(`${baseURL}/${loginType}/roles-delete/${id}?t=${new Date().getTime()}`,{
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response) => {
+            setAllRoles(allRoles?.filter(role => +role?.id !== +id));
+            toast.success(response?.data?.message);
+        }).catch(error=>{
+            toast.error(error?.response?.data?.message);
+        });
+    };
+    console.log(allRoles)
+
 
     return (
         <div className='myDefaultRoles__handler'>
@@ -208,7 +92,6 @@ let counter = 0;
                             <span>{role?.name}</span>
                         </h3>
                         <div>
-                            {/* <button className='updateRuleBtn' onClick={() => handleUpdateRule(role?.id)}>Update Role</button> */}
                             <button className="deleteRuleBtn" onClick={() => handleDeleteThisRule(role?.id)}>Remove Role</button>
                         </div>
                     </div>
@@ -226,9 +109,9 @@ let counter = 0;
                                 <table className="table table-responsive">
                                     <tbody>
                                         {loadingPermissions ? 
-                                            <div class="permissionsLoader"></div>
+                                            <div className="permissionsLoader"></div>
                                         :
-                                            permissions[0]?.[role?.id]?.map((perm , idx) =>{
+                                            permissions?.map((perm ) =>{
                                                 return(
                                                 (perm?.enable === true) &&
                                                 <tr key={perm?.id} className='onePermission__handler'>
@@ -240,8 +123,7 @@ let counter = 0;
                                                             
                                                                 <input
                                                                     type="checkbox"
-                                                                    checked={true}
-                                                                    // onChange={() => togglePermission(perm?.id , perms?.permissionId)}
+                                                                    defaultChecked
                                                                 />
                                                                 <span className="slider"></span>
                                                             </label>
